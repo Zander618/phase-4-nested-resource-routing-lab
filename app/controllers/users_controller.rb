1fdad923ac1c9d834a9648e36b670738ed052102
Zachary Sirna <zsirna@yahoo.com>
2022-11-27 16:10:17 -0500

    commit tree 385e66898dfd912398abe21827be913988132871

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,41 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  def index_user_items
+    user = User.find(params[:user_id])
+    user_items = user.items
+    render json: user_items
+  end 
+
+  def items
+    user = User.find(params[:user_id])
+    user_items = user.items
+    items = user_items.find(params[:id])
+    render json: items
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    user_items = user.items
+    item = user_items.create(item_params)
+    render json: item, status: :created
+  end
+
+  # /users/:user_id/items
+
+  private
+  
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: { error: "Dog house not found" }, status: :not_found
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+  
   resources :users, only: [:show]
+  resources :items, only: [:index]
+
+  get '/users/:user_id/items', to: 'users#index_user_items' 
+  get '/users/:user_id/items/:id', to: "users#items"
+  post "/users/:user_id/items", to: "users#create"
+
 end
